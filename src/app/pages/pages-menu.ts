13dd92f9ba8d18a1336a3f774b736897a7828ff3
Farhang <farhang.darzi@gmail.com>
2018-06-12 18:38:32 +0430

    Implement the menu items.

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,31 +1,143 @@
 import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
+
   {
-    title: 'داشبورد',
-    icon: 'nb-home',
-    link: '/pages/dashboard',
-    home: true,
-  },
-  {
-    title: 'کاربر',
-    icon: 'nb-locked',
+    title: 'مدیریت تولید',
+    expanded: true,
+
     children: [
       {
-        title: 'ورود',
-        link: '/auth/login',
+        icon: 'fa fa-user',
+        title: 'برنامه ریزی تولید',
+        link: '/',
       },
       {
-        title: 'ثبت نام',
-        link: '/auth/register',
+        icon: 'ion-ios-person',
+        title: 'آمار تولید',
+        link: '/',
       },
       {
-        title: 'درخواست رمز عبور جدید',
-        link: '/auth/request-password',
+        icon: 'nb-person',
+        title: 'کنترل تولید',
+        link: '/',
+      },
+    ],
+  },
+  {
+    title: 'انبار داری',
+    expanded: true,
+    children: [
+      {
+        icon: 'nb-person',
+        title: 'ورود کالا',
+        link: 'stocking/inputParts',
       },
       {
-        title: 'پسورد ریست',
-        link: '/auth/reset-password',
+        icon: 'nb-keypad',
+        title: 'خروج کالا',
+        link: '/',
+      },
+      {
+        icon: 'nb-keypad',
+        title: 'گزارشات',
+        children: [
+          {
+            icon: 'nb-person',
+            title: 'کارتکس',
+            link: '/',
+          },
+          {
+            icon: 'nb-keypad',
+            title: 'موجودی',
+            link: '/',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'تعریف اطلاعات پایه',
+    expanded: true,
+    children: [
+      {
+        icon: 'nb-person',
+        title: 'انبار',
+        children: [
+          {
+            icon: 'nb-person',
+            title: 'انبار اصلی',
+            link: 'stocking/MainStockCreate',
+          },
+          {
+            icon: 'nb-keypad',
+            title: 'انبار فرعی',
+            link: '/',
+          }
+        ],
+      },
+      {
+        icon: 'nb-keypad',
+        title: 'کالا',
+        children: [
+          {
+            icon: 'nb-person',
+            title: 'تعریف کالا',
+            link: '/',
+          },
+          {
+            icon: 'nb-keypad',
+            title: 'تعریف گروه کالا',
+            link: '/',
+          },
+          {
+            icon: 'nb-keypad',
+            title: 'تعریف روش تولید',
+            link: '/',
+          },
+          {
+            icon: 'nb-keypad',
+            title: 'تغریف درخت محصول',
+            link: '/',
+          }
+        ],
+      },
+      {
+        icon: 'nb-keypad',
+        title: 'طرف حساب',
+        children: [
+          {
+            icon: 'nb-person',
+            title: 'تعریف طرف حساب',
+            link: '/',
+          },
+          {
+            icon: 'nb-keypad',
+            title: 'تعریف گروه طرف حساب',
+            link: '/',
+          }
+        ],
+      },
+      {
+        icon: 'nb-keypad',
+        title: 'پرسنل',
+        children: [
+          {
+            icon: 'nb-person',
+            title: 'تعریف پرسنل',
+            link: '/',
+          },
+          {
+            icon: 'nb-keypad',
+            title: 'تعریف گروه پرسنل',
+            link: '/',
+          }
+        ],
+      },
+      {
+        icon: 'nb-keypad',
+        title: 'تعریف دسترسی ها',
+        link: '/'
       },
     ],
   },
